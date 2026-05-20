05229d761d8766bce5381a5c5688c60150de541b
shaharban-banu <sonushaharban44@gmail.com>
2026-05-20 12:32:32 +0530

    feat:added Prometheus and Grafan monitoring

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 from app.routes.ticket_routes import (router)
 from app.database.database import engine
 from app.database.models import Base
@@ -6,6 +7,8 @@ from app.database.models import Base
 Base.metadata.create_all(bind=engine)
 
 app=FastAPI(title="Customer Support AI")
+
+Instrumentator().instrument(app).expose(app)
 
 app.include_router(router)
 
